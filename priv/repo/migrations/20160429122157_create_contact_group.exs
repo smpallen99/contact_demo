326f978fadf1a9b8438f21fb3d45afbbd462e784
Stephen M. Pallen <smpallen99@yahoo.com>
2016-04-29 11:31:46 -0400

    updated the on_delete in migrations

--- priv/repo/migrations/20160429122157_create_contact_group.exs
+++ priv/repo/migrations/20160429122157_create_contact_group.exs
@@ -3,8 +3,8 @@ defmodule Nested.Repo.Migrations.CreateContactGroup do
 
   def change do
     create table(:contacts_groups) do
-      add :contact_id, references(:contacts, on_delete: :nothing)
-      add :group_id, references(:groups, on_delete: :nothing)
+      add :contact_id, references(:contacts, on_delete: :delete_all)
+      add :group_id, references(:groups, on_delete: :delete_all)
     end
     create index(:contacts_groups, [:contact_id])
     create index(:contacts_groups, [:group_id])
--- priv/repo/migrations/20160429134017_create_contact_phone_number.exs
+++ priv/repo/migrations/20160429134017_create_contact_phone_number.exs
@@ -3,8 +3,8 @@ defmodule Nested.Repo.Migrations.CreateContactPhoneNumber do
 
   def change do
     create table(:contacts_phone_numbers) do
-      add :contact_id, references(:contacts, on_delete: :nothing)
-      add :phone_number_id, references(:phone_numbers, on_delete: :nothing)
+      add :contact_id, references(:contacts, on_delete: :delete_all)
+      add :phone_number_id, references(:phone_numbers, on_delete: :delete_all)
 
       timestamps
     end
