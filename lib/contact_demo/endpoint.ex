f0acf3b2f1fba0e3fc612c5fbe1196b6218d66d6
Stephen M. Pallen <smpallen99@yahoo.com>
2016-07-19 20:58:36 -0400

    remove themes from endpoint

--- lib/contact_demo/endpoint.ex
+++ lib/contact_demo/endpoint.ex
@@ -9,7 +9,7 @@ defmodule ContactDemo.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :contact_demo, gzip: false,
-    only: ~w(css fonts images js themes favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
